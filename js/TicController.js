bf8ab513a3389d065ab41d11070cba387678cc64
Conrad Sze <conradsze@gmail.com>
2015-04-02 15:34:02 -0700

    added the find winner logic

--- js/TicController.js
+++ js/TicController.js
@@ -8,22 +8,56 @@ function TodosController() {
 	self = this;
 	self.box = [];
 	self.nextmove = "x";
+	self.movecount = 0;
 	self.click = click;
+	self.getwinner=getwinner;
+
 
 	for (var i = 0; i < 9; i++) {
 		self.box.push({
-			text: ""
+			text: "", value: Math.pow(2,i)
 		})
 	};
 
 	function click(t) {
 		index = self.box.indexOf(t);
+		if (self.box[index].text== "") {
 		self.box[index].text = self.nextmove;
+		self.movecount++
+		self.getwinner()
 		if (self.nextmove == "x") {
 			self.nextmove = "o"
 		} else {
 			self.nextmove = "x"
 		};
+		};
+
+
+	}
+
+	function getwinner(){
+
+		if ((self.box[0].text!="" && self.box[0].text==self.box[1].text && self.box[1].text==self.box[2].text)||
+			(self.box[0].text!="" && self.box[0].text==self.box[3].text && self.box[3].text==self.box[6].text)||
+			(self.box[0].text!="" && self.box[0].text==self.box[4].text && self.box[4].text==self.box[8].text)||
+			(self.box[1].text!="" && self.box[1].text==self.box[4].text && self.box[4].text==self.box[7].text)||
+			(self.box[2].text!="" && self.box[2].text==self.box[4].text && self.box[4].text==self.box[6].text)||
+			(self.box[2].text!="" && self.box[2].text==self.box[5].text && self.box[5].text==self.box[8].text)||
+			(self.box[3].text!="" && self.box[3].text==self.box[4].text && self.box[4].text==self.box[5].text)||
+			(self.box[6].text!="" && self.box[6].text==self.box[7].text && self.box[7].text==self.box[8].text)
+			){
+
+			alert(self.nextmove+" win!")
+;
+		}
+		else if(self.movecount==9){
+			setTimeout(function() {
+
+			alert("tie")
+
+		}, 0);
+		}
+	
 
 	}
 
