d09c8592a8fa316eaeb61132efbe3a8478fb1500
Conrad Sze <conradsze@gmail.com>
2015-04-03 09:31:33 -0700

    added the gameover feature

--- js/TicController.js
+++ js/TicController.js
@@ -1,63 +1,68 @@
 angular
 	.module('ticApp')
-	.controller("TicController", TodosController);
+	.controller("TicController", TicController);
 
-// TicController.$inject = ['$firebaseArray'];
+TicController.$inject = ['$firebase'];
 
-function TodosController() {
+
+
+function TicController($firebase) {
 	self = this;
 	self.box = [];
 	self.nextmove = "x";
 	self.movecount = 0;
 	self.click = click;
-	self.getwinner=getwinner;
+	self.getwinner = getwinner;
+	self.gameover = false;
 
 
 	for (var i = 0; i < 9; i++) {
 		self.box.push({
-			text: "", value: Math.pow(2,i)
+			text: "",
+			value: Math.pow(2, i)
 		})
 	};
 
 	function click(t) {
 		index = self.box.indexOf(t);
-		if (self.box[index].text== "") {
-		self.box[index].text = self.nextmove;
-		self.movecount++
-		self.getwinner()
-		if (self.nextmove == "x") {
-			self.nextmove = "o"
-		} else {
-			self.nextmove = "x"
-		};
+		if ((self.box[index].text == "")&&(self.gameover==false)) {
+			self.box[index].text = self.nextmove;
+			self.movecount++
+				self.getwinner()
+			if (self.nextmove == "x") {
+				self.nextmove = "o"
+			} else {
+				self.nextmove = "x"
+			};
 		};
 
 
 	}
 
-	function getwinner(){
+	function getwinner() {
 
-		if ((self.box[0].text!="" && self.box[0].text==self.box[1].text && self.box[1].text==self.box[2].text)||
-			(self.box[0].text!="" && self.box[0].text==self.box[3].text && self.box[3].text==self.box[6].text)||
-			(self.box[0].text!="" && self.box[0].text==self.box[4].text && self.box[4].text==self.box[8].text)||
-			(self.box[1].text!="" && self.box[1].text==self.box[4].text && self.box[4].text==self.box[7].text)||
-			(self.box[2].text!="" && self.box[2].text==self.box[4].text && self.box[4].text==self.box[6].text)||
-			(self.box[2].text!="" && self.box[2].text==self.box[5].text && self.box[5].text==self.box[8].text)||
-			(self.box[3].text!="" && self.box[3].text==self.box[4].text && self.box[4].text==self.box[5].text)||
-			(self.box[6].text!="" && self.box[6].text==self.box[7].text && self.box[7].text==self.box[8].text)
-			){
+		if ((self.box[0].text != "" && self.box[0].text == self.box[1].text && self.box[1].text == self.box[2].text) ||
+			(self.box[0].text != "" && self.box[0].text == self.box[3].text && self.box[3].text == self.box[6].text) ||
+			(self.box[0].text != "" && self.box[0].text == self.box[4].text && self.box[4].text == self.box[8].text) ||
+			(self.box[1].text != "" && self.box[1].text == self.box[4].text && self.box[4].text == self.box[7].text) ||
+			(self.box[2].text != "" && self.box[2].text == self.box[4].text && self.box[4].text == self.box[6].text) ||
+			(self.box[2].text != "" && self.box[2].text == self.box[5].text && self.box[5].text == self.box[8].text) ||
+			(self.box[3].text != "" && self.box[3].text == self.box[4].text && self.box[4].text == self.box[5].text) ||
+			(self.box[6].text != "" && self.box[6].text == self.box[7].text && self.box[7].text == self.box[8].text)
+		) {
 
-			alert(self.nextmove+" win!")
-;
-		}
-		else if(self.movecount==9){
+			self.gameover= true;
+
+			alert(self.nextmove + " win!");
+		} else if (self.movecount == 9) {
 			setTimeout(function() {
+				self.gameover= true;
 
-			alert("tie")
+				alert("tie")
 
-		}, 0);
+			}, 0);
 		}
-	
+
 
 	}
 
